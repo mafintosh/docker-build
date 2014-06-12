12908c252e1e63ff25c02be9300366d2d1125e9e
Mathias Buus <mathiasbuus@gmail.com>
2014-06-12 03:55:37 +0200

    fixed output and more options

--- cli.js
+++ cli.js
@@ -5,22 +5,32 @@ var minimist = require('minimist')
 var tar = require('tar-fs')
 
 var argv = minimist(process.argv, {
-  boolean: 'cache',
+  boolean: ['cache', 'version'],
   alias: {
     remote: 'r',
     tag: 't',
-    help: 'h'
+    help: 'h',
+    quiet: 'q',
+    version: 'v'
   }
 })
 
 if (argv.help) {
   console.error(
-    'docker-build [folder?] [options]\n'+
-    ' --remote, -r  [docker-host]\n'+
-    ' --tag,    -t  [tag]\n'+
-    ' --no-cache\n'
+    'Usage: docker-build [folder?] [options]\n'+
+    '\n'+
+    '  --remote,  -r  [docker-host]\n'+
+    '  --tag,     -t  [tag]\n'+
+    '  --quiet,   -q\n'+
+    '  --version, -v\n'+
+    '  --no-cache\n'
   )
   process.exit(1)
+}
+
+if (argv.version) {
+  console.log(require('./package').version)
+  process.exit(0)
 }
 
 var onerror = function(err) {
@@ -28,4 +38,8 @@ var onerror = function(err) {
   process.exit(2)
 }
 
-tar.pack(argv._[2] || '.').on('error', onerror).pipe(build(argv.remote, argv)).on('error', onerror)
+tar.pack(argv._[2] || '.')
+  .on('error', onerror)
+  .pipe(build(argv.remote, argv))
+  .on('error', onerror)
+  .pipe(process.stdout)
