ee4f51842ec75f89be5d18c65ca95813c7dd78c2
Mathias Buus <mathiasbuus@gmail.com>
2014-07-09 01:53:08 +0200

    using docker-remote-api

--- index.js
+++ index.js
@@ -1,46 +1,40 @@
 var duplexify = require('duplexify')
-var host = require('docker-host')
-var concat = require('concat-stream')
-var http = require('http-https')
 var parse = require('through-json')
-var xtend = require('xtend')
-var querystring = require('querystring')
+var docker = require('docker-remote-api')
 
 var build = function(remote, opts) {
-  opts = opts ? xtend(host(remote), opts) : host(remote)
+  if (arguments.length === 1 && typeof remote === 'object') return build(null, remote)
+  if (!opts) opts = {}
+
+  var request = remote instanceof docker ? remote : docker(remote)
 
   var qs = {}
   if (opts.tag) qs.t = opts.tag
   if (opts.cache === false) qs.nocache = 'true'
   if (opts.quiet) qs.q = 'true'
 
-  var req = http.request(xtend(opts, {
-    method: 'POST',
-    path: '/v1.12/build?'+querystring.stringify(qs),
+  var req = request.post('/build', {
+    qs: qs,
+    registry: opts.registry,
     headers: {
-      'Content-Type': 'application/tar',
-      'X-Registry-Config': new Buffer(JSON.stringify(opts.registry || {})+'\n').toString('base64')
+      'Content-Type': 'application/tar'
     }
-  }))
+  }, function(err, res) {
+    if (err) return dup.destroy(err)
+    dup.setReadable(res.pipe(parse(function(data) {
+      if (!data.error) return data.stream
+      dup.destroy(new Error(data.error.trim()))
+    })))
+  })
 
   var dup = duplexify(req)
 
-  var onerror = function(message) {
-    dup.destroy(new Error(message.toString().trim()))
-  }
-
-  var ondata = function(data) {
-    if (!data.error) return data.stream
-    dup.destroy(new Error(data.error.trim()))
-  }
-
-  req.on('response', function(res) {
-    if (res.statusCode !== 200) return res.pipe(concat(onerror))
-    dup.setReadable(res.pipe(parse(ondata)))
+  req.on('error', function(err) {
+    dup.destroy(err)
   })
 
   req.on('close', function() {
-    dup.destroy()
+    dup.destroy(new Error('Premature close'))
   })
 
   return dup
